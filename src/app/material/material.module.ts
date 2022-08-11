5947a9fb04b7c7b675ec3613622cae03023ee96c
Salvador Peña <salvadorignacio23@gmail.com>
2022-08-11 18:17:19 -0400

    Se crea estructura Base

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+
+@NgModule({
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ]
+})
+export class MaterialModule { }
