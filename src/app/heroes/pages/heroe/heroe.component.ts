b6d058bf671e51934dcf104cf3ff036d7c76ab38
Salvador Peña <salvadorignacio23@gmail.com>
2022-09-23 17:03:19 -0300

    Se añade buscar heroe por id

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { switchMap } from 'rxjs';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
+  styles: [`
+    img{
+      width: 100%;
+      border-radius: 5px;
+    }
+  `
   ]
 })
 export class HeroeComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  heroe!: Heroe;
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private heroesService: HeroesService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.activatedRoute.params
-    .subscribe(({id})=> console.log(id));
+    .pipe(
+      switchMap(({id})=>this.heroesService.getHeroePorId(id))
+    )
+    .subscribe(heroe =>this.heroe = heroe);
+  }
+
+  regresar(){
+    this.router.navigate(['/heroes/listado']);
   }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,16 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Heroe } from '../interfaces/heroes.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
+  private baseUrl: string = environment.baseUrl;
 
   constructor(private http: HttpClient) { }
 
   getHeroes(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`);
+  }
+
+  getHeroePorId(id: string): Observable<Heroe> {
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
   }
 }
